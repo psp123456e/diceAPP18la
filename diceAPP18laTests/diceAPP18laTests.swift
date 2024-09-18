bd13b4761346959cbe36229fbbdc23c7d78dfd80
psp123456e <pspdsl123@gmail.com>
2024-09-19 05:02:30 +0800

    Initial Commit

---
+++ diceAPP18laTests/diceAPP18laTests.swift
@@ -0,0 +1,17 @@
+//
+//  diceAPP18laTests.swift
+//  diceAPP18laTests
+//
+//  Created by 蔡宜潔 on 2024/9/19.
+//
+
+import Testing
+@testable import diceAPP18la
+
+struct diceAPP18laTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
